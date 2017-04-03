b579f2b70359880d514fbe62930c3ae118863fd6
Lewis Kabui <lewis.maina@andela.com>
2017-04-03 21:26:32 +0300

    (feature) Create NoteViewSet

--- api/viewsets.py
+++ api/viewsets.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 
 from rest_framework import viewsets
 
-from api import serializers
+from api import serializers, models
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -11,3 +11,11 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
+
+
+class NoteViewSet(viewsets.ModelViewSet):
+    """
+    Viewset to enable CRUD operations on the User resource.
+    """
+    queryset = models.Notes.objects.all()
+    serializer_class = serializers.NoteSerializer
