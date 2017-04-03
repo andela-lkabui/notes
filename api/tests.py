7e5f907048f8474411a1af57daedb9115c6a6092
Lewis Kabui <lewis.maina@andela.com>
2017-04-03 20:32:27 +0300

    (feature) Create tests for PUT on user resource

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,5 @@
+import json
+
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
@@ -8,6 +10,18 @@ class UserResourceTest(TestCase):
     """
     Tests the CRUD methods in user resource.
     """
+    def create_user(self):
+        """
+        helper method for creating users
+        """
+        user_create_url = reverse('user-list')
+        # first create a user
+        user = {
+            "username": "testuser",
+            "password": "12345"
+        }
+        response = self.client.post(user_create_url, user)
+        return user
 
     def test_user_can_create_profile(self):
         """
@@ -57,3 +71,19 @@ class UserResourceTest(TestCase):
         response = self.client.get(user_get_detail)
         self.assertEqual(response.status_code, 200)
         self.assertTrue('testuser' in response.content.decode('ascii'))
+
+    def test_user_can_edit_user_detail(self):
+        """
+        test that a user can edit user details
+        """
+        user = self.create_user()
+        user_obj = User.objects.filter(username=user['username'])[0]
+        user_put_detail = reverse('user-detail', kwargs={'pk': user_obj.id})
+        new_data = {
+            'username': "testuser",
+            'password': "67890"
+        }
+        json_data = json.dumps(new_data)
+        response = self.client.put(
+            user_put_detail, json_data, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
