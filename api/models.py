c209179e666ee3d0c01959a5eb778cfd3f0b278b
Lewis Kabui <lewis.maina@andela.com>
2017-04-03 20:52:33 +0300

    (feature) Create Notes model

--- api/models.py
+++ api/models.py
@@ -1,5 +1,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Notes(models.Model):
+    """
+    ORM for notes
+    """
+    title = models.CharField(max_length=100, blank=False)
+    note = models.CharField(max_length=140, blank=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
