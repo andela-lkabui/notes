7ae746153a4f981592a680a50a11d5a2061c36e2
Lewis Kabui <lewis.maina@andela.com>
2017-04-03 21:26:01 +0300

    (feature) Create NoteSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 
 from rest_framework import serializers
 
+from api import models
+
 
 class UserSerializer(serializers.ModelSerializer):
     """
@@ -11,3 +13,12 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('username', )
         write_only_fields = ('password',)
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    """
+    Serializes the User object
+    """
+    class Meta:
+        model = models.Notes
+        fields = ('title', 'note', 'owner')
