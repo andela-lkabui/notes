701b4fdf954e61283beca017cb54d0c4f5688699
Lewis Kabui <lewis.maina@andela.com>
2017-04-03 20:12:56 +0300

    (feature) Create tests for POST operation

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class UserResourceTest(TestCase):
+    """
+    Tests the CRUD methods in user resource.
+    """
+
+    def test_user_can_create_profile(self):
+        """
+        Test that user can perform a POST operation on this resource.
+        """
+        user_create_url = reverse('user-list')
+
+        user = {
+            "username": "testuser",
+            "password": "12345"
+        }
+        response = self.client.post(user_create_url, user)
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue('username' in response.content.decode('ascii'))
+        self.assertTrue(user['username'] in response.content.decode('ascii'))
+        
