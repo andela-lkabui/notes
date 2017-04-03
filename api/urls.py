edf08917d2048ede325d0c9950c797e8101a4f6f
Lewis Kabui <lewis.maina@andela.com>
2017-04-03 20:13:36 +0300

    (feature) Implement User resource

--- api/urls.py
+++ api/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from rest_framework import routers
+
+from api import viewsets
+
+router = routers.SimpleRouter()
+router.register('users', viewsets.UserViewSet)
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('api.urls')),
+    url(r'^', include(router.urls)),
 ]
