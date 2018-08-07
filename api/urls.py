b821d252ddc3811b957aeec894e8cf624d36e34a
Lewis Kabui <lewis_maina@yahoo.com>
2018-08-07 23:07:40 +0300

    (feature) Include the '/api/docs/' url

--- api/urls.py
+++ api/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 from rest_framework import routers
+from rest_framework.documentation import include_docs_urls
 
 from rest_framework_jwt.views import obtain_jwt_token
 
@@ -28,5 +29,6 @@ router.register('notes', viewsets.NoteViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'api-token-auth/', obtain_jwt_token, name='api-auth')
+    url(r'api-token-auth/', obtain_jwt_token, name='api-auth'),
+    url(r'^docs/', include_docs_urls(title='My API title')),
 ]
