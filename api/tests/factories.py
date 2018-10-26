fa4df156bcd20c6b330510cdebba48743bd7d83e
Lewis Kabui <lewis.maina@andela.com>
2018-10-26 14:33:09 +0300

    (fix) Override factory create() method
    - Override of the create method was necessiated by the need to hash
      user passwords as the factory creates the object

--- api/tests/factories.py
+++ api/tests/factories.py
@@ -8,9 +8,13 @@ class UserFactory(factory.django.DjangoModelFactory):
     first_name = factory.Faker('first_name')
     last_name = factory.Faker('last_name')
     username = factory.LazyAttribute(lambda a: '{0}.{1}'.format(a.first_name, a.last_name).lower())
-    # password = factory.LazyAttribute(lambda a: a.set_password(factory.Faker('password')))
+    password = factory.Faker('password')
     email = factory.LazyAttribute(lambda a: '{0}.{1}@notesapi.com'.format(a.first_name, a.last_name).lower())
 
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        """Override the default ``_create`` with our custom call."""
+        return model_class.objects.create_user(**kwargs)
 
 class NoteFactory(factory.Factory):
     class Meta:
