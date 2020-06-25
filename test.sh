57254ca2a68a18c8dbf2cf83660fbc6c0e626973
Lewis Kabui <lewisemm@users.noreply.github.com>
2020-06-25 14:49:48 +0300

    Return appropriate exit code when tests fail

--- test.sh
+++ test.sh
@@ -3,5 +3,11 @@
 # run the tests
 docker-compose run -e ENVIRONMENT=TESTING api
 
+exit_code_a=$?
+
 # clean up afterwards
 docker-compose down
+
+exit_code_b=$?
+
+exit $((exit_code_a + exit_code_b))
